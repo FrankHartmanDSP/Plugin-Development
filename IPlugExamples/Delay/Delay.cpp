4ab7f21204080f6d765058d8581a9c7926baf288
hartm433 <hartm433@umn.edu>
2019-04-17 12:51:29 -0500

    Added ability to change "wet" effect vol. level

--- IPlugExamples/Delay/Delay.cpp
+++ IPlugExamples/Delay/Delay.cpp
@@ -26,7 +26,7 @@ Delay::Delay(IPlugInstanceInfo instanceInfo)
   TRACE;
 
   //arguments are: name, defaultVal, minVal, maxVal, step, label
-  GetParam(kFeedback)->InitDouble("Feedback", 50.0, 0.0, 99.9, 0.01, "%");
+  GetParam(kFeedback)->InitDouble("Feedback", 50.0, 0.0, 100.0, 0.01, "%");
   GetParam(kFeedback)->SetShape(0.1);
 
   IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
@@ -55,8 +55,7 @@ void Delay::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrame
 
 	  for (int s = 0; s < nFrames; ++s, ++input, ++output) {
 		  // Writing data to left and right channel buffers for delay effect
-		  
-		  if (i == 0 ) {
+		  if (i == 0) {
 			  if (ptrL > 32767) {
 				  ptrL = 0;
 				  firstIteration = false;
@@ -65,58 +64,36 @@ void Delay::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrame
 			  // Delay effect
 			  if (!firstIteration) {
 				  if (mBufferL[DelayPtrCalculation(&ptrL)]) {
-					  *output = *input + mBufferL[DelayPtrCalculation(&ptrL)];
+					  *output = *input + (mFeedback * mBufferL[DelayPtrCalculation(&ptrL)]);
 				  }
 				  else {
 					  *output = *input;
 				  }
-
 			  }
 			  else {
 				  *output = *input;
 			  }
 			  ptrL++;
 		  } else {
-			  
 			  if (ptrR > 32767) {
 				  ptrR = 0;
 				  firstIteration = false;
 			  }
 			  mBufferR[ptrR] = *input;
 			  if (!firstIteration) {
-				  if (mBufferR[DelayPtrCalculation(&ptrR)]) { //unhandled read exception
-					  *output = *input + mBufferR[DelayPtrCalculation(&ptrR)];
+				  if (mBufferR[DelayPtrCalculation(&ptrR)]) {
+					  *output = *input + (mFeedback * mBufferR[DelayPtrCalculation(&ptrR)]);
 				  }
 				  else {
 					  *output = *input;
 				  }
-
 			  }
 			  else {
 				  *output = *input;
 			  }
 			  ptrR++;
-			  
 		  }
-		  
-
-		  
-		  
-		  /**
-		  if (*input >= 0) {
-			  *output = fmin(*input, mFeedback);
-		  } else {
-			  *output = fmax(*input, -mFeedback);
-		  }
-		  *output /= mFeedback;
-		  */
-
-		  
-		  
-		  //*output = *input;
 	  }
-
-	  
   }
 }
 
@@ -133,7 +110,7 @@ void Delay::OnParamChange(int paramIdx)
   switch (paramIdx)
   {
     case kFeedback:
-      mFeedback = 1 - (GetParam(kFeedback)->Value() / 100.);
+      mFeedback = (GetParam(kFeedback)->Value() / 100.);
       break;
 
     default:
