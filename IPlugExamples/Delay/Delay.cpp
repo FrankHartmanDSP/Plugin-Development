bd0d27c3c578d599b417fdcc0630502d32a28abd
hartm433 <hartm433@umn.edu>
2019-04-17 12:37:50 -0500

    Delay plugin function (left speaker only)

--- IPlugExamples/Delay/Delay.cpp
+++ IPlugExamples/Delay/Delay.cpp
@@ -7,7 +7,7 @@ const int kNumPrograms = 3;
 
 enum EParams
 {
-  kThreshold = 0,
+  kFeedback = 0,
   kNumParams
 };
 
@@ -15,26 +15,26 @@ enum ELayout
 {
   kWidth = GUI_WIDTH,
   kHeight = GUI_HEIGHT,
-  kThresholdX = 79,
-  kThresholdY = 62,
+  kFeedbackX = 79,
+  kFeedbackY = 62,
   kKnobFrames = 128
 };
 
 Delay::Delay(IPlugInstanceInfo instanceInfo)
-  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mThreshold(1.)
+  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mFeedback(1.), ptrL(0), ptrR(0), firstIteration(true)
 {
   TRACE;
 
   //arguments are: name, defaultVal, minVal, maxVal, step, label
-  GetParam(kThreshold)->InitDouble("Threshold", 0.0, 0.0, 99.9, 0.01, "%");
-  GetParam(kThreshold)->SetShape(0.1);
+  GetParam(kFeedback)->InitDouble("Feedback", 50.0, 0.0, 99.9, 0.01, "%");
+  GetParam(kFeedback)->SetShape(0.1);
 
   IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
   pGraphics->AttachBackground(BACKGROUND_ID, BACKGROUND_FN);
 
   IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
 
-  pGraphics->AttachControl(new IKnobMultiControl(this, kThresholdX, kThresholdY, kThreshold, &knob));
+  pGraphics->AttachControl(new IKnobMultiControl(this, kFeedbackX, kFeedbackY, kFeedback, &knob));
 
   AttachGraphics(pGraphics);
 
@@ -54,13 +54,67 @@ void Delay::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrame
 	  double* output = outputs[i];
 
 	  for (int s = 0; s < nFrames; ++s, ++input, ++output) {
-		  if (*input >= 0) {
-			  *output = fmin(*input, mThreshold);
+		  // Writing data to left and right channel buffers for delay effect
+		  
+		  if (i == 0) {
+			  if (ptrL > 32767) {
+				  ptrL = 0;
+				  firstIteration = false;
+			  }
+			  mBufferL[ptrL] = *input;
+			  // Delay effect
+			  if (!firstIteration) {
+				  if (mBufferL[DelayPtrCalculation(&ptrL)]) {
+					  *output = *input + mBufferL[DelayPtrCalculation(&ptrL)];
+				  }
+				  else {
+					  *output = *input;
+				  }
+
+			  }
+			  else {
+				  *output = *input;
+			  }
 		  } else {
-			  *output = fmax(*input, -mThreshold);
+			  /**
+			  if (ptrR > 32767) {
+				  ptrR = 0;
+				  firstIteration = false;
+			  }
+			  mBufferR[ptrR] = *input;
+			  if (!firstIteration) {
+				  if (mBufferR[DelayPtrCalculation(&ptrR)]) { //unhandled read exception
+					  *output = *input + mBufferR[DelayPtrCalculation(&ptrR)];
+				  }
+				  else {
+					  *output = *input;
+				  }
+
+			  }
+			  else {
+				  *output = *input;
+			  }
+			  */
 		  }
-		  *output /= mThreshold;
+		  
+
+		  
+		  
+		  /**
+		  if (*input >= 0) {
+			  *output = fmin(*input, mFeedback);
+		  } else {
+			  *output = fmax(*input, -mFeedback);
+		  }
+		  *output /= mFeedback;
+		  */
+
+		  ptrL++;
+		  ptrR++;
+		  //*output = *input;
 	  }
+
+	  
   }
 }
 
@@ -76,8 +130,8 @@ void Delay::OnParamChange(int paramIdx)
 
   switch (paramIdx)
   {
-    case kThreshold:
-      mThreshold = 1 - (GetParam(kThreshold)->Value() / 100.);
+    case kFeedback:
+      mFeedback = 1 - (GetParam(kFeedback)->Value() / 100.);
       break;
 
     default:
@@ -86,7 +140,15 @@ void Delay::OnParamChange(int paramIdx)
 }
 
 void Delay::CreatePresets() {
-	MakePreset("Clean", 0.0);
+	MakePreset("Default", 50.0);
 	MakePreset("Subtle", 20.0);
-	MakePreset("Extreme", 100);
+	MakePreset("Extreme", 99.9);
+}
+
+int Delay::DelayPtrCalculation(int * ptr) {
+	if ((*ptr - 16384) < 0) {
+		return (32768 + (*ptr - 16384));
+	} else {
+		return (*ptr - 16384);
+	}
 }
--- IPlugExamples/Delay/Delay.h
+++ IPlugExamples/Delay/Delay.h
@@ -12,10 +12,19 @@ public:
   void Reset();
   void OnParamChange(int paramIdx);
   void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+  int DelayPtrCalculation(int * ptr);
 
 private:
-  double mThreshold;
+  double mFeedback;
+  //int mSampleRate; // How to access if changed from 44.1kHz? 
+  // -> Unnecessary, GetSampleRate() is inherited from IPlugBase
+  int ptrL;
+  int ptrR;
+  double mBufferL[32768]; // 2^15 is 32768
+  double mBufferR[32768]; // 2^15
   void CreatePresets();
+  bool firstIteration; // Replace with logic
+
 };
 
 #endif
